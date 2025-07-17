6c5ff9bf745e27adaaa69cdf1d0b4c2202006d46
lincheewei <cheewei1998@gmail.com>
2025-07-17 15:19:33 +0800

    added new sharp

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const net = require('net');
+const sharp = require('sharp');
+
 const { spawn, execSync } = require('child_process');
 
 const app = express();
